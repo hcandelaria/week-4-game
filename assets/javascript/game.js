5a677b645a6a1c9118d21978b50e6237c9c115bf
hcandelaria <hcandelaria66.7@gmail.com>
2017-09-04 22:12:45 -0400

    game.js- added var player and enemy tested them out. added a gameObject with a attack funtion to calculate the hp.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
+	//Initialize array to hold all characters
 	var charactersArr = [];
+	//Initialize the player
+	var player;
+	//Initialize the enemy
+	var enemy;
+	//Game Object
+	var gameObject = {
+
+		attack: function(){
+			//Attack enemy
+			enemy.setHP(enemy.getHP() - player.getAP());
+			//Enemy counter
+			player.setHP((player.getHP() - enemy.getCP()));
+			//Player gets strong
+			player.setAP((player.getAP() + player.getCP()));
+		},
+		pickEnemy: function(){
+		}
+		pickPlayer: function(){
+		}
+	};
+	//Initialize characters
 	for(var i = 0; i < 4; i++){
 		//Character object
 		var character = {
@@ -11,14 +33,14 @@ $(document).ready(function(){
 			attackPower: 0,
 			//Counter power
 			counterPower: 0,
-			//Set values for character
+			//constructor
 			createCharacter: function(name,hp,ap,cp){
 				this.characterName = name;
 				this.healthPoints = hp;
 				this.attackPower = ap;
 				this.counterPower = cp;
 			},
-			//Get healthPoints
+			//Get characterName
 			getCN: function(){
 				return this.characterName;
 			},
@@ -27,18 +49,45 @@ $(document).ready(function(){
 				return this.healthPoints;
 			},
 			//Get attackPower
-			getHP: function(){
+			getAP: function(){
 				return this.attackPower;
 			},
 			//Get counterPower
-			getHP: function(){
+			getCP: function(){
 				return this.counterPower;
+			},
+			//set characterName
+			setCN: function(x){
+				this.characterName = x;
+			},
+			//set healthPoints
+			setHP: function(x){
+				this.healthPoints = x;
+			},
+			//set attackPower
+			setAP: function(x){
+				this.attackPower = x;
+			},
+			//set counterPower
+			setCP: function(x){
+				this.counterPower = x;
 			}
 		};
-
+		//push chracter to the character array
 		charactersArr.push(character);
+		
 	}
-	charactersArr[0].createCharacter("test",50,10,10);
-	console.log(charactersArr[0]);
+//T E S T   G A M E
+	charactersArr[0].createCharacter("test1",50,10,10);
+	charactersArr[1].createCharacter("test2",70,8,8);
+	charactersArr[2].createCharacter("test3",40,15,15);
+	charactersArr[3].createCharacter("test4",80,5,5);
 
+	player = charactersArr[0];
+	enemy = charactersArr[2];
+
+	gameObject.attack();
+
+	console.log(player);
+	console.log(player);
 });
